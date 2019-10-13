a5f189d9fde501c599162b5bba1cbe429b6aca93
Chengzhu Zhang <zhang40@llnl.gov>
2019-10-12 21:00:33 -0700

    add cmip related py and cfg

---
+++ run_e3sm_diags_CO2.py
@@ -0,0 +1,42 @@
+import os
+from acme_diags.parameter.core_parameter import CoreParameter
+from acme_diags.parameter.area_mean_time_series_parameter import AreaMeanTimeSeriesParameter
+from acme_diags.run import runner
+
+param = CoreParameter()
+
+#For compy
+#machine_path_prefix = '/compyfs/e3sm_diags_data/'
+#For cori
+#machine_path_prefix = '/global/project/projectdirs/acme/acme_diags'
+#for acme1
+machine_path_prefix = '/p/user_pub/e3sm/e3sm_diags_data/'
+
+#param.reference_data_path = os.path.join(machine_path_prefix, 'obs_for_e3sm_diags/time-series/')
+#param.test_data_path = os.path.join(machine_path_prefix, 'test_model_data_for_acme_diags/time-series/CESM1-CAM5_cmip/')
+param.reference_data_path = '/p/user_pub/e3sm/zhang40/e3sm_cmip6_xmls/v1_water_cycle/abrupt-4xCO2/'
+param.test_data_path = '/p/user_pub/e3sm/zhang40/e3sm_cmip6_xmls/v1_water_cycle/1pctCO2/r1i1p1f1'
+param.test_name = 'e3sm_v1_1pctCO2_abrupt4CO2'
+
+#For compy
+#prefix = '/compyfs/www/zhan429/doc_examples/'
+#For cori
+#prefix = '/global/project/projectdirs/acme/www/zhang40/'
+#For acme1
+prefix = '/var/www/acme/acme-diags/zhang40/tests/'
+param.results_dir = os.path.join(prefix, 'area_mean_co2_r1')
+param.multiprocessing = True
+param.num_workers =  25
+
+# We're passing in this new object as well, in
+# addition to the CoreParameter object.
+
+ts_param = AreaMeanTimeSeriesParameter()
+ts_param.ref_names = ['r1i1p1f1']   #This setting plot model data only
+ts_param.start_yr = '0001'
+ts_param.end_yr = '0150'
+
+runner.sets_to_run = ['area_mean_time_series']
+runner.run_diags([param, ts_param])
+
+
